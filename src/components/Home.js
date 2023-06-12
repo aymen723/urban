d7ea9e52387f22ddddceae811244728cd3d13686
aymen723 <benmohammed.a.a@gmail.com>
2023-06-12 14:42:56 +0100

    working on the Home page

---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Home.css";
+// import logo from "./components/images/logo.png";
+function Home() {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="headbar">
+          <div className="buttons">
+            <img src="/components/images/logo.png"></img>
+            <a>Home</a>
+            <a>About Us</a>
+            <a>Service</a>
+            <a>Blog</a>
+            <a>Portfolio</a>
+            <a>Contact Us</a>
+          </div>
+          <div className="Sign">
+            <button className="btn">Sign Up</button>
+          </div>
+        </div>
+        <div className="content">
+          <div className="titles">
+            <h3>Nous vous aidons</h3>
+            <h1>À Construire Votre </h1>
+            <h3>Étude De Faisabilité</h3>
+            <button></button>
+          </div>
+          <div className="images">
+            <img src=""></img>
+          </div>
+        </div>
+      </div>
+
+      <div className="block_2"></div>
+    </div>
+  );
+}
+
+export default Home;
